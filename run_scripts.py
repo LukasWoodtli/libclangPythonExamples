db350091726bc98a78535a3f9d3f8afb9c14f932
woodtluk <woodtluk@e17a30e0-a4a0-48a1-9280-7d2f855b2862>
2011-08-30 13:57:31 +0000

    Tests für libclang Phython bindings angepasst.
    git-svn-id: https://xp-dev.com/svn/woodtluk-myStuff@248 e17a30e0-a4a0-48a1-9280-7d2f855b2862

--- run_scripts.py
+++ run_scripts.py
@@ -2,17 +2,17 @@
 
 """ Tutorial taken from http://eli.thegreenplace.net/2011/07/03/parsing-c-in-python-with-clang/
     scripts need to be able to find the clang.cindex which resides in 
-    '{$llvm-repository-root}\tools\clang\bindings\python\clang' (you need to checkout llvm and clang
-    add '{$llvm-repository-root}\llvm\tools\clang\bindings\python\' to PYTHONPATH environment variable
+    '${llvm-repository-root}\tools\clang\bindings\python\clang' (you need to checkout llvm and clang
+    add '${llvm-repository-root}\llvm\tools\clang\bindings\python\' to PYTHONPATH environment variable
     the binding must find the libclang library on Windows set the PATH environment variable to find
     libclang.dll on on Linux set the LD_LIBRARY_PATH environment library to find libclang.so
-    the library should be found in '{$llvm-build-directory}\bin\Release'
+    the library should be found in '${llvm-build-directory}\bin\' or one of its subdirectories
  """
 
 
 import findTyperefs
  
-#findTyperefs.main(['room.cpp', 'Person'])
+findTyperefs.main(['room.cpp', 'Person'])
 
 
 import findFunctions
